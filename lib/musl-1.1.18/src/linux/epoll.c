b916b0f704148891739f13b0ad0e8e264ccc2749
weiwang999 <wwang88@stevens.edu>
2019-08-01 14:19:21 -0400

    update for redis ARM

--- lib/musl-1.1.18/src/linux/epoll.c
+++ lib/musl-1.1.18/src/linux/epoll.c
@@ -1,7 +1,11 @@
+
+/* updated for Popcorn by Antonio Barbalace, Stevens 2019 */
+
 #include <sys/epoll.h>
 #include <signal.h>
 #include <errno.h>
 #include "syscall.h"
+#include <string.h>
 
 int epoll_create(int size)
 {
@@ -17,16 +21,62 @@ int epoll_create1(int flags)
 	return __syscall_ret(r);
 }
 
+#ifdef __AARCH64EL__
+struct __epoll_event {
+    unsigned long events;
+    unsigned long data;
+};
+#endif
+
 int epoll_ctl(int fd, int op, int fd2, struct epoll_event *ev)
 {
+#ifdef __AARCH64EL__
+
+    int retval; struct __epoll_event __ev; __ev.events=0; __ev.data=0;
+    __ev.events = ((unsigned int*)ev)[0];
+    __ev.data = (unsigned long*)((((unsigned int*)ev) +1))[0];
+
+    retval = syscall(SYS_epoll_ctl, fd, op, fd2, &__ev);
+    return retval;
+
+
+#else
 	return syscall(SYS_epoll_ctl, fd, op, fd2, ev);
+#endif
 }
 
 int epoll_pwait(int fd, struct epoll_event *ev, int cnt, int to, const sigset_t *sigs)
 {
-	int r = __syscall(SYS_epoll_pwait, fd, ev, cnt, to, sigs, _NSIG/8);
+#ifdef __AARCH64EL__
+//	struct __epoll_event __ev; __ev.events=0; __ev.data=0;
+	int i;
+	struct __epoll_event __ev[32];
+	memset(&__ev, 0, sizeof(struct __epoll_event)*32);
+
+	int r = __syscall(SYS_epoll_pwait, fd, &__ev, (cnt > 32) ? 32 : cnt, 
+		to, sigs, _NSIG/8);
+ //       ev->events = __ev.events;
+//	((unsigned long*)(((unsigned int*)ev)+1))[0] = __ev.data; //force cast
+
+	for (i=0; (i< 32) && (i< cnt); i++) {
+		ev[i].events = __ev[i].events;
+		((unsigned long*)(((unsigned int*)&ev[i])+1))[0] = __ev[i].data;
+	}
+#else
+    int r = __syscall(SYS_epoll_pwait, fd, ev, cnt, to, sigs, _NSIG/8);
+#endif
+    
 #ifdef SYS_epoll_wait
-	if (r==-ENOSYS && !sigs) r = __syscall(SYS_epoll_wait, fd, ev, cnt, to);
+	if (r==-ENOSYS && !sigs) {
+ #ifdef __AARCH64EL__
+		__ev.events=0; __ev.data=0;
+        r = __syscall(SYS_epoll_wait, fd, &__ev, cnt, to);        
+        ev->events = __ev.events;
+        ev->data.u64 = __ev.data;
+ #else       
+        r = __syscall(SYS_epoll_wait, fd, ev, cnt, to);
+ #endif
+	}
 #endif
 	return __syscall_ret(r);
 }
